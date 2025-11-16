e46e6bc8bda91b735179c20a98b1e10fde40f761
zaqxs123456 <56287342+zaqxs123456@users.noreply.github.com>
2025-11-16 22:44:05 +0800

    init commit

---
+++ project_init_core.py
@@ -0,0 +1,10 @@
+from managers.config_manager import ConfigManager
+
+class ProjectInitCore:
+    
+    def __init__(self):
+        self.cm = ConfigManager()
+        self.config = self.cm.config.project_init_core
+        
+    def display_module_name(self):
+        print("Module Name:", self.config.module_name)
